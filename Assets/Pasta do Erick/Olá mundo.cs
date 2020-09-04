6ebef12b22a2a5de1739093c8e08eeb1bda1464a
Erick Ribeiro Araujo <61511798+Crekpozer@users.noreply.github.com>
2020-09-03 22:52:32 -0300

    Update olá mundo.cs

--- Assets/Pasta do Erick/Olá mundo.cs
+++ Assets/Pasta do Erick/Olá mundo.cs
@@ -7,7 +7,7 @@ public class Olámundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Olá mundo!");
     }
 
     // Update is called once per frame
