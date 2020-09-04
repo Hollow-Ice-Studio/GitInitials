f4aa69770feeb04c3af6b566f6236b98ec1fbe07
germano <germanosp11@hotmail.com>
2020-09-03 23:06:31 -0300

    Consertado problemas de integração com arquivo olamundo do erick

--- Assets/Pastinha/Olamundo.cs
+++ Assets/Pastinha/Olamundo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Olámundo : MonoBehaviour
+public class Olamundo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
